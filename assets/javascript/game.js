e1e79cb5ef979de7c6107c014ac7786ac40ccc73
Ha <victorha@gmail.com>
2018-05-01 15:12:13 -0400

    problem with event listener, probably

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,19 @@
-var Chen = {HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"}
-var CM = {HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"}
-var NS = {HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"}
-var VS = {HP: 525, attack: 39, counterAttack: 47, spawn: "../images/VS_spawn.mp3"}
+var Chen = {HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
+var CM = {HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
+var NS = {HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
+var VS = {HP: 525, attack: 39, counterAttack: 47, spawn: "../images/VS_spawn.mp3"};
 var attackCount = 0;
 
+
+
 $("#heroes").on("click", ".hero", function() {
-    if ($("#radiant").is(":empty")) { // this line needs work
-        $(".hero").appendTo("#radiant") //find jquery line to move, else create and delete
+    console.log("2")
+    if ($("#radiant").is(":empty")) {
+        console.log("1");
+        //$(".hero").appendTo("#radiant") 
         audio.Play(this.spawn);
     }
-    else if //(($("#radiant").is(":empty")) && 
+    else if //(!($("#radiant").is(":empty")) && 
         ($("#dire").is(":empty")) {
         //move #hero to dire
         audio.play(this.spawn);
