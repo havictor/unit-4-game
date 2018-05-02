fe0a7507951f7a482d630b3534e639ff8d6428f3
Ha <victorha@gmail.com>
2018-05-02 15:29:18 -0400

    Fixed audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,18 @@
-var Chen = {Name: "Chen", HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
-var CM = {Name: "Crystal Maiden", HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
-var NS = {Name: "Night Stalker", HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
-var VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 4700, spawn: "../images/VS_spawn.mp3"};
+var Chen = {Name: "Chen", HP: 650, attack: 48, counterAttack: 50, spawn: new Audio("./assets/images/Chen_spawn.mp3")};
+var CM = {Name: "Crystal Maiden", HP: 524, attack: 44, counterAttack: 38, spawn: new Audio("./assets/images/CM_spawn.mp3")};
+var NS = {Name: "Night Stalker", HP: 718, attack: 61, counterAttack: 65, spawn: new Audio("./assets/images/NS_spawn.mp3")};
+var VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 4700, spawn: new Audio("./assets/images/VS_spawn.mp3")};
+var fight = new Audio("./assets/images/fight.mp3")
+var victory = {radiant: new Audio("./assets/images/RadiantVictory.mp3"), dire: new Audio("./assets/images/DireVictory.mp3")}
 var radiant = {};
+var pick = {begin: new Audio("./assets/images/Welcome.mp3"), radiant: new Audio("./assets/images/RadiantPick.mp3"), dire: new Audio("./assets/images/DirePick.mp3")}
 var dire = {};
 var attackCount = 0;
 var enemyCount = 3;
 
 $(document).ready(function() {
     $("#New").hide();
+    pick.begin.play();
     $("#Chen").data("data-stats", Chen);
     $("#CM").data("data-stats", CM);
     $("#NS").data("data-stats", NS);
@@ -20,56 +24,57 @@ $(document).ready(function() {
         if ($("#radiant").contents().length == 0) {
             $(this).appendTo(("#radiant"))
             radiant = $(this).data("data-stats");
-            //audio.Play(radiant.spawn);
+            radiant.spawn.play();
         }
 
          else if ($("#dire").contents().length == 0) {
             $(this).appendTo(("#dire"))
             dire = $(this).data("data-stats");
-            //audio.play(dire.spawn);
+            dire.spawn.play()
     }
-/*
+
 
         else {
-            //audio.play("../images/fight.mp3")
+            fight.play()
         }
     });
-
-    */
-    })
 });
-
 
 $(document).ready(function() {
     $("#attack").click(function() {
         if ($("#radiant").contents().length == 0) {
             alert("Choose Your Hero");
+            pick.radiant.play();
     }
         else if ($("#dire").contents().length == 0) {
             alert("Choose Your Enemy");
+            pick.dire.play();
     }
         else {
             attackCount++;
             dire.HP = (dire.HP - (radiant.attack * attackCount));
             if (dire.HP > 0) {
-                alert("You attacked "+dire.Name+" for "+(radiant.attack * attackCount)+ " damage, and now "+dire.Name+" has "+dire.HP+" health remaining.")
+                alert("You attacked "+dire.Name+" for "+(radiant.attack * attackCount)+ " damage, and now "+dire.Name+" has "+dire.HP+" health remaining.");
+                $("")
             }
             else if (dire.HP <= 0) {
                 alert("You attacked "+dire.Name+" for "+(radiant.attack * attackCount)+ " damage, and has been defeated")
                 $("#dire").contents().appendTo("#defeated")
                 enemyCount--
-            }
+            };
             radiant.HP = (radiant.HP - dire.counterAttack);
-            if (radiant.HP > 0 && ("#dire").contents().length == 0) {
+            if (radiant.HP > 0 && $("#dire").contents().length !== 0) {
                 alert(dire.Name+" attacked back for "+dire.counterAttack+", you have "+radiant.HP+" remaining.")
-            }
+            };
             if (enemyCount == 0) {
                 alert ("Radiant Victory!");
+                victory.radiant.play();
                 $("#New").show();
             }
-            else {
+            else if (radiant.HP <= 0) {
                 alert(dire.Name+" attacked back for "+dire.counterAttack+", and has slain you.")
                 alert("Dire Victory")
+                victory.dire.play();
                 $("#New").show();
                 $("#radiant").contents().appendTo("#defeated");
                 $("#heroes").contents().appendTo("#defeated");
@@ -78,18 +83,21 @@ $(document).ready(function() {
     });
 });
 
-
-
 $(document).ready(function() {
     $("#New").click(function() {
         $("#defeated").contents().appendTo("#heroes");
         $("#dire").contents().appendTo("#heroes");
         $("#radiant").contents().appendTo("#heroes");
         $("#New").hide();
-        Chen = {Name: "Chen", HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
-        CM = {Name: "Crystal Maiden", HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
-        NS = {Name: "Night Stalker", HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
-        VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 4700, spawn: "../images/VS_spawn.mp3"};
+        pick.begin.play();
+        Chen = {Name: "Chen", HP: 650, attack: 48, counterAttack: 50, spawn: new Audio("./assets/images/Chen_spawn.mp3")};
+        CM = {Name: "Crystal Maiden", HP: 524, attack: 44, counterAttack: 38, spawn: new Audio("./assets/images/CM_spawn.mp3")};
+        NS = {Name: "Night Stalker", HP: 718, attack: 61, counterAttack: 65, spawn: new Audio("./assets/images/NS_spawn.mp3")};
+        VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 4700, spawn: new Audio("./assets/images/VS_spawn.mp3")};
+        $("#Chen").data("data-stats", Chen);
+        $("#CM").data("data-stats", CM);
+        $("#NS").data("data-stats", NS);
+        $("#VS").data("data-stats", VS);
         radiant = {};
         dire = {};
         attackCount = 0;
@@ -97,5 +105,6 @@ $(document).ready(function() {
     })
 });
 
-//dynamic numbers of hp
+//to do: dynamic numbers of hp
+//to do: balance stats of heroes
 //to do: add juke option if time is available.
