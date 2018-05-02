859ec08dfa292bdfeaf8e8bc6a682d54ee5d8023
Ha <victorha@gmail.com>
2018-05-02 14:39:11 -0400

    newGame function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 var Chen = {Name: "Chen", HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
 var CM = {Name: "Crystal Maiden", HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
 var NS = {Name: "Night Stalker", HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
-var VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 47, spawn: "../images/VS_spawn.mp3"};
+var VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 4700, spawn: "../images/VS_spawn.mp3"};
 var radiant = {};
 var dire = {};
 var attackCount = 0;
-var selected;
 var enemyCount = 3;
 
 $(document).ready(function() {
-    $("#Chen").data("data-stats", Chen)
-    $("#CM").data("data-stats", CM)
-    $("#NS").data("data-stats", NS)
-    $("#VS").data("data-stats", VS)
+    $("#New").hide();
+    $("#Chen").data("data-stats", Chen);
+    $("#CM").data("data-stats", CM);
+    $("#NS").data("data-stats", NS);
+    $("#VS").data("data-stats", VS);
 });
 
 $(document).ready(function() {
@@ -56,7 +56,7 @@ $(document).ready(function() {
             }
             else if (dire.HP <= 0) {
                 alert("You attacked "+dire.Name+" for "+(radiant.attack * attackCount)+ " damage, and has been defeated")
-                $("#dire").contents().appendTo("#defeated") //dire hero to variable, to appendTo on new game creation
+                $("#dire").contents().appendTo("#defeated")
                 enemyCount--
             }
             radiant.HP = (radiant.HP - dire.counterAttack);
@@ -65,10 +65,12 @@ $(document).ready(function() {
             }
             if (enemyCount == 0) {
                 alert ("Radiant Victory!");
+                $("#New").show();
             }
             else {
                 alert(dire.Name+" attacked back for "+dire.counterAttack+", and has slain you.")
                 alert("Dire Victory")
+                $("#New").show();
                 $("#radiant").contents().appendTo("#defeated");
                 $("#heroes").contents().appendTo("#defeated");
             };
@@ -76,9 +78,24 @@ $(document).ready(function() {
     });
 });
 
-function newGame() {
-    $("defeated").contents().appendTo("#heroes");
-}
 
-//to do: newGame function for resetting
+
+$(document).ready(function() {
+    $("#New").click(function() {
+        $("#defeated").contents().appendTo("#heroes");
+        $("#dire").contents().appendTo("#heroes");
+        $("#radiant").contents().appendTo("#heroes");
+        $("#New").hide();
+        Chen = {Name: "Chen", HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
+        CM = {Name: "Crystal Maiden", HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
+        NS = {Name: "Night Stalker", HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
+        VS = {Name: "Vengeful Spirit", HP: 525, attack: 39, counterAttack: 4700, spawn: "../images/VS_spawn.mp3"};
+        radiant = {};
+        dire = {};
+        attackCount = 0;
+        enemyCount = 3;
+    })
+});
+
+//dynamic numbers of hp
 //to do: add juke option if time is available.
