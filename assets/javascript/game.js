138efa91ed37ceb5b2534661ee5df15566bbed3b
Ha <victorha@gmail.com>
2018-05-02 11:44:31 -0400

    Attack Conditional added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
-var Chen = {HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
-var CM = {HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
-var NS = {HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
-var VS = {HP: 525, attack: 39, counterAttack: 47, spawn: "../images/VS_spawn.mp3"};
+const Chen = {HP: 650, attack: 48, counterAttack: 50, spawn: "../images/Chen_spawn.mp3"};
+const CM = {HP: 524, attack: 44, counterAttack: 38, spawn: "../images/CM_spawn.mp3"};
+const NS = {HP: 718, attack: 61, counterAttack: 65, spawn: "../images/NS_spawn.mp3"};
+const VS = {HP: 525, attack: 39, counterAttack: 47, spawn: "../images/VS_spawn.mp3"};
+var radiant = {};
+var dire = {};
 var attackCount = 0;
-var selected
+var selected;
+var enemyCount = 4;
+
+$(document).ready(function() {
+    $("#Chen").data(Chen)
+});
 
 $(document).ready(function() {
     $("#heroes").on("click", ".hero", function() {
         if ($("#radiant").contents().length == 0) {
             $(this).appendTo(("#radiant"))
+            radiant = this.data;
             //audio.Play(this.spawn);
         }
 
@@ -23,29 +31,39 @@ $(document).ready(function() {
             //audio.play("../images/fight.mp3")
         }
     });
-    
+
     */
     })
 });
 
 
 
-/*
 
-    //if no hero, put into radiant. play sound.
-    //else put into  dire, play sound.
 
-    // if hero in radiant && no hero in dire
-    //on click, add hero to dire
 
 $("#attack").on("click", function() {
-    ($(""))
+    attackCount++;
+    dire.hp = dire.hp - radiant.attack;
+    if (dire.hp <= 0) {
+        //delete dire hero or move and hide
+        enemyCount--
+    }
+    else {
+        radiant.hp = radiant.hp - dire.counterAttack
+        if (radiant.hp <= 0) {
+            alert("Game Over")
+        }
+    };
+
+    if (enemyCount = 0) {
+        alert ("You win!");
+    }
 });
+
+
 //button on click, hero radiant <<attacks>>
 //dire <<counterattacks>>
 
 //if dire HP <= 0, status = hidden
 
 //if radiant <= 0, game over
-
-*/
